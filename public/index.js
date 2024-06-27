ada36c28ed194e9e1ac28a984780bd48aa9909a7
62630619 <sumanadasrh@gmail.com>
2024-06-28 00:08:24 +0530

    CSR

--- public/index.js
+++ public/index.js
@@ -1,33 +1,33 @@
-let ul=document.querySelector('#ul')
+let ul = document.querySelector('#ul')
 
 
-function restart(){
-    $.get('/data',(data)=>{
+function restart() {
+    $.get('/data', (data) => {
         $("#ul").empty()
-        for(let i in data){
-            let li=document.createElement('li')
-           li.textContent=data[i]
-           ul.appendChild(li)
-         
-        //    $('ul').append(`<li>${data[i]}</li>`) its also done by jquery
+        for (let i in data) {
+            let li = document.createElement('li')
+            li.textContent = data[i]
+            ul.appendChild(li)
+
+            //    $('ul').append(`<li>${data[i]}</li>`) its also done by jquery
 
         }
     })
 
-    
+
 }
 restart()
-$('#btn').on('click',()=>{
+$('#btn').on('click', () => {
 
-let inp=$('#num').val();
-console.log(inp,"sdjf");
-$.post('/data',{inp},(res)=>{
-    console.log(res,"yhi hai");
+    let inp = $('#num').val();
+    console.log(inp, "sdjf");
+    $.post('/data', { inp }, (res) => {
+        console.log(res, "yhi hai");
         //    $('ul').append(`<li>${inp}</li>`)// its also done by jquery
-$('#num').val('');
-     
+        $('#num').val('');
+
         restart()
-})
+    })
 })
 
 // function send(){
